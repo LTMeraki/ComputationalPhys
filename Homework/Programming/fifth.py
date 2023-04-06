e287430c01032b574b30c5ffa0f94fcab499450c
1Meraki <lacatrianam@gmail.com>
2023-04-06 15:28:40 -0500

    Modify output messages

--- Homework/Programming/fifth.py
+++ Homework/Programming/fifth.py
@@ -16,16 +16,16 @@ a, b = float(input("Enter the extreme left: ")), float(input("Enter the extreme 
 
 # Check if the function has a root at the interval's extrems.
 if f(a) == 0:
-    print(f"\nThe root is ({a:.4f}, {f(a):.4e}) in the given interval.")
+    print(f"\nThe root is x = {a:.4f} in the given interval.")
 elif f(b) == 0:
-    print(f"\nThe root is ({b:.4f}, {f(b):.4e}) in the given interval.")
+    print(f"\nThe root is x = {b:.4f} in the given interval.")
 else:
     # Apply the bisection method for the given number of iterations.
     for i in range(n):
         m = (a + b) / 2
         # If the function evaluates to zero at the midpoint, it is a root.
         if f(m) == 0:
-            print(f"\nThe root is in ({m:.4f}, {f(m):.4e}) in the given interval.\n")
+            print(f"\nThe root is in x = {m:.4f} in the given interval.\n")
             quit()
         elif f(a) * f(m) < 0:
             # Root in [a, m] if the product of the function at a and m is negative.
@@ -38,4 +38,4 @@ else:
             print("\nThere is no root in this interval.")
             quit()
 
-    print(f"\nThe root is in ({m:.4f}, {f(m):.4e}) in the given interval.\n")
+    print(f"\nThe root is in x = {m:.4f} in the given interval.\n")
